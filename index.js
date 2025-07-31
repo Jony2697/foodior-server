9582de026e81a64ca223b64da8d4f49074af7da7
Jony2697 <ijony2697@gmail.com>
2025-07-31 13:50:47 +0600

    foodDetails api added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const port =process.env.PORT || 3000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 //middleware
@@ -32,6 +32,13 @@ async function run() {
     // foods api
     app.get('/foods',async(req,res)=>{
       const result=await foodsCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.get('/foodsDetails/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query={_id:new ObjectId(id)};
+      const result=await foodsCollection.findOne(query);
       res.send(result);
     })
 
